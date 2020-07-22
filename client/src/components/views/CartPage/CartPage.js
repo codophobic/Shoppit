27fe892dc109b2663915373c15c20ae779faf4d4
ANKIT <mr.ankityo111@gmail.com>
2020-07-23 01:33:44 +0530

    added cart page

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -1,8 +1,14 @@
 import React,{useState,useEffect} from 'react'
 import {useDispatch} from 'react-redux';
-import {getCartItems} from '../../../_actions/user_actions';
+import {getCartItems,removeCartItem} from '../../../_actions/user_actions';
+import UserCardBlock from './Sections/UserCardBlock';
+import {Result,Empty} from 'antd';
+import Axios from 'axios';
 function CartPage(props) {
    const dispatch= useDispatch();
+  const [Total, setTotal] = useState(0);
+  const [ShowTotal, setShowTotal] = useState(false);
+  const [ShowSuccess, setShowSuccess] = useState(false);
     useEffect(() => {
        let cartItems=[];
        if(props.user.userData&&props.user.userData.cart){
@@ -16,9 +22,72 @@ function CartPage(props) {
            }
        }
     }, [props.user.userData])
+
+    useEffect(()=>{
+       if(props.user.cartDetail&&props.user.cartDetail.length>0)
+       {
+         calTotal(props.user.cartDetail);
+       }
+    },[props.user.cartDetail])
+
+    const calTotal=(cartDetail)=>{
+        let total=0;
+
+        cartDetail.map(item=>{
+            total+=parseInt(item.price,10)*item.quantity;
+        });
+        setTotal(total);
+        setShowTotal(true);
+    }
+
+    const removeFromcart=(id)=>{
+      
+        dispatch(removeCartItem(id)).
+        then(()=>{
+        //    Axios.get('/api/users/userCartInfo').then(res=>{
+        //        if(res.data.success)
+        //        {
+        //          if(res.data.cartDetail.length<=0)
+        //          setShowTotal(false);
+        //          else
+        //          calTotal(res.data.cartDetail);
+        //        }
+        //        else{
+        //            alert('failed to get cart info');
+        //        }
+        //    })
+        })
+    }
     return (
-        <div>
+        <div style={{width:'85%',margin:'3rem auto'}}>
+            <h1>MY Cart</h1>
+            <div>
+            <UserCardBlock
+            products={props.user.cartDetail}
+            removeItem={removeFromcart}/>
             
+            
+            {
+                ShowTotal?
+                <div style={{marginTop:'3rem'}}>
+                <h2>Total Amount:Rs. {Total} </h2>
+            </div>
+            :
+            ShowSuccess?
+            <Result
+            status="success"
+           title="Successfully Purchased Items"          
+          />:
+          <div style={{width:'100%',display:'flex',flexDirection:'column',justifyContent:"center"}}>
+          <br/>
+          <Empty description={false}/>
+          <p>No items in the Cart</p>
+          </div>
+            
+          }
+          
+          
+          </div>
         </div>
     )
 }
